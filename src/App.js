ccf594172337ac4f1a8863f1079781c9265dc7c8
Raj Sree <shanrajsree@gmail.com>
2020-09-11 21:40:51 -0600

    Added features to table. 1.Sorting and reverse-sorting done. 2. Searching/Filtering in the table done. 3. Pagination done. 4.Fetching more data done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useMemo, useState, useEffect } from "react";
 import axios from "axios";
-import TableComponent from "./components/Table";
+import TableComponent from "./components/Table.js";
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-
 
   const columns = useMemo(
     () => [
@@ -28,23 +28,26 @@ const App = () => {
   );
     //data state to store the books API data. Its initial value is an empty array
    const [data, setData] = useState([]);
+   const searchtopic = 'react';
+   const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+   const url = `${API_ENDPOINT}${searchtopic}`
 
     // Using useEffect to call the API once mounted and set the data.
     useEffect(() => {
       (async () => {
-        let result = await axios("https://hn.algolia.com/api/v1/search?query=react");
+        let result = await axios(url);
         setData(result.data.hits);
-        console.log("result analusis :", result.data.hits);
+        console.log("result analysis :", result.data.hits);
       })();
     }, []);
-  
+
   const stories = [
     {
       title: "React",
       url: "https://reactjs.org/",
       author: "Jordan Walke",
       num_comments: 3,
-      points: 4,
+      points: 1,
       objectID: 0,
     },
     {
@@ -63,36 +66,60 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
+  const [searchList, setSearchList] = React.useState("");
+
+  const handleChangeinList = (event) => {
+    setSearchList(event.target.value);
+  };
+  let size = 0;
+  const fetchmore = (event) => {
+    console.log("Fetching more data");
+    size = size + 20;
+    (async () => {
+      let result = await axios("https://hn.algolia.com/api/v1/search?query=/size=" +size);
+      setData(result.data.hits);
+      console.log("More data fetched :", result.data.hits);
+    })();
+  };
+
   return (
     <div>
       <h1>Connectus Global</h1>
-
+      <div className="basepage">
+      <div className="searchgrid">
+      <h3>Search input</h3>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
-
       <p>
         Searching for <strong>{searchTerm}</strong>.
       </p>
-
-      <hr />
-
-      <List list={stories} />
-
+      <h3>Search in List</h3>
+      <label htmlFor="searchList">Search: </label>
+      <input id="searchList" type="text" onChange={handleChangeinList} placeholder="Search for book Title "/>
+        <List list={stories} searchList={searchList}/>
+      </div>
+      <hr /><hr />
+      <div className="tablegrid">
+      <h3>Fetch data to Table, search for title, Sorting/Reverse sorting all columns & Pagination</h3>
       <TableComponent columns={columns} data={data} />
-      
+      <button className="more" onClick={() => fetchmore()}>
+          <a className="more">Fetch More</a>
+      </button>
+      </div>
+      </div>
     </div>
   );
 };
 
 const List = (props) =>
-  props.list.map((item) => (
-    <div key={item.objectID}>
+  props.list.filter(item => item['title'].toLowerCase().indexOf(props.searchList.toLowerCase()) > -1).map((filtereditem) => (
+    <div key={filtereditem.objectID} className="listStories">
       <span>
-        <a href={item.url}>{item.title}</a>
+        <a href={filtereditem.url}>{filtereditem.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
+      <span>{filtereditem.author}</span>
+      <span>{filtereditem.num_comments}</span>
+      <span>{filtereditem.points}</span>
     </div>
   ));
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,37 +1,74 @@
-import React from "react";
-import { useTable } from "react-table";
+import React, {useState} from "react";
+import { useTable, useFilters, useSortBy, usePagination} from "react-table";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Table.css';
 
 export default function Table({ columns, data }) {
-  // Use the useTable Hook to send the columns and data to build the table
+
+  // state creation
+const [filterInput, setFilterInput] = useState("");
+
+// Updating the state on input changes
+const handleFilterChange = e => {
+  const value = e.target.value || undefined;
+  setFilter("title", value);
+  setFilterInput(value);
+};
+
+
+  // Using the useTable Hook to send the columns and data to build the table
   const {
-    getTableProps, // table props from react-table
-    getTableBodyProps, // table body props from react-table
-    headerGroups, // headerGroups, if your table has groupings
-    rows, // rows for the table based on the data passed
-    prepareRow // Prepare the row (this function needs to be called for each row before getting the row props)
+    getTableProps,
+    getTableBodyProps,
+    headerGroups, 
+    prepareRow,
+    setFilter,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
   } = useTable({
     columns,
-    data
-  });
+    data,
+    initialState: { pageIndex: 2, pageSize: 5 },
+  },
+  useFilters,
+  useSortBy,
+  usePagination);
 
-  /* 
-    Render the UI for your table
-    - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
-  */
+
   return (
+    <div>
+    <input  value={filterInput}  onChange={handleFilterChange}  placeholder={"Search book title"} />
+    <pre>
+
+     </pre>
     <table {...getTableProps()}>
       <thead>
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}
+              className={
+                column.isSorted
+                  ? column.isSortedDesc
+                    ? "sort-desc"
+                    : "sort-asc"
+                  : ""
+            }
+            >{column.render("Header")} { column.isSorted ? column.isSortedDesc ? <span> &#9660; </span> : <span>&#9650;</span> : ""}</th>
             ))}
           </tr>
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
+        {page.map((row, i) => {
           prepareRow(row);
           return (
             <tr {...row.getRowProps()}>
@@ -43,5 +80,43 @@ export default function Table({ columns, data }) {
         })}
       </tbody>
     </table>
+    <ul className="pagination">
+              <li className="page-item" onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                  <a className="page-link">First</a>
+              </li>
+              <li className="page-item" onClick={() => previousPage()} disabled={!canPreviousPage}>
+                  <a className="page-link">{'<'}</a>
+              </li>
+              <li className="page-item" onClick={() => nextPage()} disabled={!canNextPage}>
+                  <a className="page-link">{'>'}</a>
+              </li>
+              <li className="page-item" onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                  <a className="page-link">Last</a>
+              </li>
+              <li>
+                  <a className="page-link">
+                      Page{' '}
+                      <strong>
+                          {pageIndex + 1} of {pageOptions.length}
+                      </strong>{' '}
+                  </a>
+              </li>
+
+              <select
+                  className="form-control"
+                  value={pageSize}
+                  onChange={e => {
+                      setPageSize(Number(e.target.value))
+                  }}
+                  style={{ width: '120px', height: '38px' }}
+              >
+                  {[5, 10, 20].map(pageSize => (
+                      <option key={pageSize} value={pageSize}>
+                          Show {pageSize}
+                      </option>
+                  ))}
+              </select>
+          </ul>
+  </div>
   );
 }
